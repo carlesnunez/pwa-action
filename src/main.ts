2c4fd1e10cbf9631321aaadba5f0ae4238e47728
Carles Núñez Tomeo <carlesnunezt@gmail.com>
2020-03-26 13:19:28 +0100

    Push approach

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,62 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import fetch from 'node-fetch'
+import github from '@actions/github'
+
+async function pushComment(repoToken: string, comment: string): Promise<void> {
+  const {
+    payload: {pull_request: pullRequest, repository}
+  } = github.context
+
+  const repoFullName = repository?.full_name
+
+  if (!pullRequest || !repoFullName) {
+    core.error('this action only works on pull_request events')
+    core.setOutput('comment-created', 'false')
+  } else {
+    const {number: issueNumber} = pullRequest
+
+    const [owner, repo] = repoFullName.split('/')
+
+    const octokit = new github.GitHub(repoToken)
+
+    await octokit.issues.createComment({
+      owner,
+      repo,
+      issue_number: issueNumber,
+      body: comment
+    })
+
+    core.setOutput('comment-created', 'true')
+  }
+}
+function getInputs(): {repoToken: string; giphyToken: string} {
+  const repoToken: string = core.getInput('repo-token')
+  const giphyToken: string = core.getInput('giphy-token')
+
+  return {
+    repoToken,
+    giphyToken
+  }
+}
+
+async function getGifUrl(token: string): Promise<string> {
+  const res = await fetch(
+    `http://api.giphy.com/v1/gifs/random?api_key=${token}`
+  )
+
+  const {image_url: imageUrl} = await res.json()
+
+  return imageUrl
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.debug(`Initializing our action`)
+    const {repoToken, giphyToken} = getInputs()
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const randomGifUrl = await getGifUrl(giphyToken)
+
+    await pushComment(repoToken, randomGifUrl)
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
